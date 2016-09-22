57754ed0d37c3eb2ec446b132aab234f6760a94b
Jaime Neves <jaimebarrosoneves@gmail.com>
2016-09-22 18:34:38 -0400

    firts commit

---
+++ server.js
@@ -0,0 +1,27 @@
+/**
+ * Module express
+ */
+var express = require('express');
+
+/**
+ * Module serve Static
+ */
+var serveStatic = require('serve-static');
+
+var app = express();
+
+/**
+ * Check the environment of running
+ */
+var client = '/app';
+
+/**
+ * Check if the environment variable (process.env.PORT) has a value
+ */
+var port = process.env.PORT || 8081;
+
+app.use(serveStatic(__dirname + client));
+
+app.listen(port,function(){
+    console.log('localhost:'+port);
+});
